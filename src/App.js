b6e2dd557a49adddab552e7cbf1c6a28a58f3990
Fred <frederic.bellard0@gmail.com>
2025-03-28 16:43:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className="App">
+<p class="bg-blue-200">rrrrr</p>
+<Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div class="w-full h-52">
+        <div class="w-52 h-52 object-contain">
+        <img src="../assets/rachit-tank-2cFZ_FB08UM-unsplash.jpg" />
+        </div>
+        <h1>Projet</h1>
+    </div>
+  )
+}
+
+export default Hero
